0658f9285b3d4348aa4e1c15f04e6efb19dd4534
SOUMEN MAITY <100059245+SoumenM1@users.noreply.github.com>
2022-11-30 08:09:52 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,17 @@ import React from 'react';
 
 export const NavBar = () =>{
     return(
-
+    <>
+        <nav>
+        <ul>
+       <Link className = "index-link" to="/"></Link>
+        <Link className = "home-link" to="/">Home</Link>
+         
+        </ul>
+        
+        </nav>
+        
+        
+        </>
     )
 }
